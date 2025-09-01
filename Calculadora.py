05fbea17ec78c4ce989050ba772ffd6bf88fa09c
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-01 20:07:59 -0300

    Commit 5 → Adicionar divisão (com tratamento de divisão por zero).
    Commit 5 → Adicionar divisão (com tratamento de divisão por zero).

--- Calculadora.py
+++ Calculadora.py
@@ -11,3 +11,7 @@ print("Subtração:", subtracao(5, 2))
 def multiplicacao(a, b):
     return a * b
 print("Multiplicação:", multiplicacao(4, 3))
+
+def divisao(a, b):
+    return a / b if b != 0 else "Erro: divisão por zero"
+print("Divisão:", divisao(10, 2))
