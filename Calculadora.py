b0fa8d21e9523393f14a2a7fcdd0a6c914fc7136
CarlosPurcaru <carlospurcaru@gmail.com>
2025-09-01 20:07:18 -0300

    Commit 4 → Adicionar multiplicação.
    Commit 4 → Adicionar multiplicação.

--- Calculadora.py
+++ Calculadora.py
@@ -7,3 +7,7 @@ print("Soma:", soma(2, 3))
 def subtracao(a, b):
     return a - b
 print("Subtração:", subtracao(5, 2))
+
+def multiplicacao(a, b):
+    return a * b
+print("Multiplicação:", multiplicacao(4, 3))
